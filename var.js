6022a5bc3a59840de22c23b2fe3c6ea4f9cc6800
June Lee <gudtjrppo@gmail.com>
2022-01-19 22:15:54 +0900

    all file update and upload

--- var.js
+++ var.js
@@ -1,20 +1,64 @@
-// 모듈 가져오기.
-const http = require('http');
-
-// 클라이언트의 요청을 처리할 함수 선언.
-function onRequest(req, res) { // request, response는 길어서 줄여서 씀.
-    // 뒤에 올 내용이 이런 거다. 하는 헤더 설정.
-    res.writeHead(200, {'Content-Type':'text/plain'}); // 200은 상태 코드(Status Code)이다. plain은 일반 text를 의미함.
-    // 데이터 전송.
-    res.write("Hello NodeJasfasdfasfdS");
-    // 응답 종료.
-    res.end();
-}
-
-// 서버 생성.
-const server = http.createServer(onRequest); // 옵저버 패턴.
-
-// 요청 대기.
-server.listen(5000); // 5000은 포트 번호이다. 
-// 80은 널리 쓰이므로, 피하도록 하자.
-// 예시로 naver.com:80 으로 들어가면 네이버가 들어가진다.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    
+    if(pathname === '/'){
+      fs.readFile(`data/${queryData.id}`, function(err, description){
+        var template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+          </html>
+        `;
+        
+  
+        response.writeHead(200);
+        response.end(template);
+      })
+    }else{
+      var errPage = `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1>페이지를 찾을 수 없습니다.</h1>
+            <ul>
+              <li><a href="/">메인으로 돌아가기</a></li>
+              <li><a href="/?id=HTML">HTML페이지 바로가기</a></li>
+              <li><a href="/?id=CSS">CSS페이지 바로가기</a></li>
+              <li><a href="/?id=JAVASCRIPT">JAVASCRIPT페이지 바로가기</a></li>
+            </ul>
+          </body>
+        </html>
+      `;
+      response.writeHead(404);
+      response.end(errPage);
+    }
+    
+    
+    
+ 
+});
+app.listen(3000);
